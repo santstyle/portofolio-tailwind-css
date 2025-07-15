d7c3068e4b6b355bea3c0367451ce75b8489c7ad
SantStyle <134206753+santstyle@users.noreply.github.com>
2025-07-16 06:16:05 +0700

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,17 @@
 // Navbar Fixed
-window.onscroll = function() {
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#to-top');
 
-    if(window.pageYOffset > fixedNav) {
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed')
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -14,7 +19,15 @@ window.onscroll = function() {
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+// Click Out Hamburger
+window.addEventListener('click', function (e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
