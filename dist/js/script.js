a64eb51fe7bd68c611e5352b3ea012d554ffb63f
SantStyle <134206753+santstyle@users.noreply.github.com>
2025-07-16 12:03:08 +0700

    Add files via upload

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,3 +31,24 @@ window.addEventListener('click', function (e) {
         navMenu.classList.add('hidden');
     }
 });
+
+// Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function () {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark')
+        localStorage.theme = 'light';
+    }
+});
+
+//Posisi Toggle Sesuai Mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
